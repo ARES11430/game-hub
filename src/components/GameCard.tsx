89628f2eb260e11776b8d618aa189c11648fe7d9
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-05-17 19:23:16 +0330

    Building game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,19 @@
-function GameCard(): JSX.Element {
-	return <></>;
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Game } from '../hooks/useGames';
+
+interface Props {
+	game: Game;
+}
+
+function GameCard({ game }: Props): JSX.Element {
+	return (
+		<Card borderRadius={15} overflow='hidden'>
+			<Image src={game.background_image} />
+			<CardBody>
+				<Heading fontSize='2xl'>{game.name}</Heading>
+			</CardBody>
+		</Card>
+	);
 }
 
 export default GameCard;
