b6684e65794cac67ba494417012757b80cdff1a5
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-05-20 17:32:28 +0330

    change theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,79 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig, StyleFunctionProps } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-	initialColorMode: 'dark'
+	initialColorMode: 'dark',
+	useSystemColorMode: false
 };
 
-const theme = extendTheme({ config });
+const colors = {
+	gray: {
+		50: '#f9f9f9',
+		100: '#ededed',
+		200: '#d3d3d3',
+		300: '#b3b3b3',
+		400: '#a0a0a0',
+		500: '#898989',
+		600: '#6c6c6c',
+		700: '#202020',
+		800: '#121212',
+		900: '#111'
+	},
+	light: {
+		background: '#e9f7f7', // Light blue sky background
+		text: '#333', // Dark gray text
+		primary: '#247ba0', // Deep blue primary
+		secondary: '#6c757d' // Dark gray secondary
+	},
+	dark: {
+		background: '#121212', // Very dark background
+		text: '#cbd5e0', // Cool light gray text
+		primary: '#90cdf4', // Cool blue primary
+		secondary: '#718096' // Cool dark gray secondary
+	}
+};
+
+const fonts = {
+	body: "'Roboto', sans-serif",
+	heading: "'Poppins', sans-serif"
+};
+
+const components = {
+	Button: {
+		variants: {
+			solid: (props: StyleFunctionProps) => ({
+				bg: props.colorMode === 'dark' ? 'dark.primary' : 'light.primary',
+				color: 'white',
+				_hover: {
+					bg: props.colorMode === 'dark' ? 'dark.secondary' : 'light.secondary'
+				}
+			})
+		}
+	}
+};
+
+const container = {
+	baseStyle: (props: StyleFunctionProps) => ({
+		bg: props.colorMode === 'dark' ? 'dark.background' : 'light.background',
+		color: props.colorMode === 'dark' ? 'dark.text' : 'light.text'
+	})
+};
+
+const styles = {
+	global: (props: StyleFunctionProps) => ({
+		'html, body': {
+			color: props.colorMode === 'dark' ? 'dark.text' : 'light.text',
+			bg: props.colorMode === 'dark' ? 'dark.background' : 'light.background'
+		}
+	})
+};
+
+const theme = extendTheme({
+	config,
+	colors,
+	fonts,
+	components,
+	container,
+	styles
+});
 
 export default theme;
