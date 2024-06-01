3e24a0217a11a083546219820752e34868ddd969
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-01 16:51:24 +0330

    static platforms
    add platforms as static data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import platformService from '../services/platformService';
+import platforms from '../data/platforms';
 
 const usePlatforms = () =>
 	useQuery({
 		queryKey: ['platforms'],
 		queryFn: platformService.getAll,
-		staleTime: 24 * 60 * 60 * 1000
+		staleTime: 24 * 60 * 60 * 1000,
+		initialData: platforms
 	});
 
 export default usePlatforms;
