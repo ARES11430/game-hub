e899c3084fb8cb237d72d4d4fee3dff37a437259
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-01 14:04:24 +0330

    re-factoring using apiClient

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export default axios.create({
+const axiosInstance = axios.create({
 	baseURL: 'https://api.rawg.io/api',
 	params: {
 		key: import.meta.env.VITE_API_KEY
@@ -11,3 +11,17 @@ export interface FetchResponse<T> {
 	count: number;
 	results: T[];
 }
+
+class APIClient<T> {
+	constructor(private endpoint: string) {}
+
+	getAll = async (confit: AxiosRequestConfig): Promise<FetchResponse<T>> => {
+		return await axiosInstance.get<FetchResponse<T>>(this.endpoint).then((res) => res.data);
+	};
+
+	post = async (data: T): Promise<T> => {
+		return await axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+	};
+}
+
+export default APIClient;
