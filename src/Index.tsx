8a85d88b664e63cd63cf79dbf2d2d654f75ed2cf
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-04 18:30:18 +0330

    create routes

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -8,7 +9,8 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
 import theme from './theme.ts';
 
-import App from './App.tsx';
+// * router
+import router from './routes.tsx';
 
 const client = new QueryClient({
 	defaultOptions: {
@@ -28,7 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<ChakraProvider theme={theme}>
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<QueryClientProvider client={client}>
-				<App />
+				<RouterProvider router={router} />
 				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</ChakraProvider>
---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,5 @@
+const GameDetailPage = () => {
+	return <p>Game detail Page</p>;
+};
+
+export default GameDetailPage;
