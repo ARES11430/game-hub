f9983ed412dcc938eec967282f69ff88638ffa04
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-06-07 14:38:33 +0330

    add genre list for smaller devices

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import { Box, Grid, Show, GridItem, HStack } from '@chakra-ui/react';
-
+import { Box, Grid, Show, GridItem, HStack, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 import GameGrid from '../components/GameGrid';
 import GameHeading from '../components/GameHeading';
 import GenresList from '../components/GenresList';
@@ -7,33 +7,49 @@ import PlatformSelector from '../components/PlatformSelector';
 import SortSelector from '../components/SortSelector';
 
 const HomePage = () => {
+	const [showAside, setShowAside] = useState(false);
+
+	const toggleAside = () => setShowAside(!showAside);
+
 	return (
-		<Grid
-			templateAreas={{
-				base: `'main'`,
-				lg: `'aside main'`
-			}}
-			templateColumns={{
-				base: '1fr',
-				lg: '250px 1fr'
-			}}
-		>
-			<Show above='lg'>
-				<GridItem area='aside' paddingX={5}>
-					<GenresList />
-				</GridItem>
+		<Box>
+			<Show below='lg'>
+				<Button onClick={toggleAside} marginX={showAside ? 5 : 3} marginBottom={4}>
+					{showAside ? 'Hide Genres' : 'Show Genres'}
+				</Button>
 			</Show>
-			<GridItem area='main'>
-				<Box paddingLeft='13px'>
-					<GameHeading />
-					<HStack spacing={5} marginBottom={2}>
-						<PlatformSelector />
-						<SortSelector />
-					</HStack>
-				</Box>
-				<GameGrid />
-			</GridItem>
-		</Grid>
+			<Grid
+				templateAreas={{
+					base: showAside ? `'aside main'` : `'main'`,
+					lg: `'aside main'`
+				}}
+				templateColumns={{
+					base: showAside ? '250px 1fr' : '1fr',
+					lg: '250px 1fr'
+				}}
+			>
+				<Show above='lg'>
+					<GridItem area='aside' paddingX={5}>
+						<GenresList />
+					</GridItem>
+				</Show>
+				{showAside && (
+					<GridItem area='aside' paddingX={5} display={{ lg: 'none' }}>
+						<GenresList />
+					</GridItem>
+				)}
+				<GridItem area='main'>
+					<Box paddingLeft={{ base: '13px', lg: 0 }}>
+						<GameHeading />
+						<HStack spacing={5} marginBottom={2}>
+							<PlatformSelector />
+							<SortSelector />
+						</HStack>
+					</Box>
+					<GameGrid />
+				</GridItem>
+			</Grid>
+		</Box>
 	);
 };
 
